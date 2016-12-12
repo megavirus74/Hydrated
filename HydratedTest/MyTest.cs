791f64dc501101d0ef70815cb28ddb314b5e2ebe
Волков Иван Алексеевич <volkoivan74@gmail.com>
2016-12-12 05:35:03 +0300

    Added other tests

--- HydratedTest/MyTest.cs
+++ HydratedTest/MyTest.cs
@@ -15,5 +15,42 @@ namespace HydratedTest
 		{
 			Assert.AreEqual(page.drops.Count, 10);
 		}
+
+		[Test]
+		public void UpdateCountTest()
+		{
+			page.UpdateWaterCount(5);
+			Assert.AreEqual(page.waterCount, 5);
+		}
+
+		[Test]
+		public void ResetTest()
+		{
+			page.OnResetPressed(null, null);
+			Assert.AreEqual(page.waterCount, 0);
+		}
+
+		[Test]
+		public void OnDrinkPressedTest()
+		{
+			page.OnResetPressed(null, null);
+			page.OnDrinkPressed(null, null);
+			Assert.AreEqual(page.waterCount, 1);
+		}
+
+		[Test]
+		public void ImageChangeTest()
+		{
+			page.UpdateWaterCount(1);
+			var image = page.drops[0];
+			if (image.Source is Xamarin.Forms.FileImageSource)
+			{
+				Xamarin.Forms.FileImageSource objFileImageSource = (Xamarin.Forms.FileImageSource)image.Source;
+				//
+				// Access the file that was specified:-
+				string strFileName = objFileImageSource.File;
+				Assert.AreEqual(strFileName, "WaterFilled");
+			}
+		}
 	}
 }
